95cd2602e2b8cec77c1727ebcb607be43c82b802
Alexis Tanaka <atanaka@peerplays.com>
2022-06-22 11:02:00 +0000

    #721 Validation Rule Update For Generating Bitcoin Address

--- src/actions/forms/errorsHandling/checkDepositPublicKey.js
+++ src/actions/forms/errorsHandling/checkDepositPublicKey.js
@@ -1,6 +1,7 @@
 export const checkDepositPublicKey = ({depositPublicKey}) => {
     if(depositPublicKey.match(/^  *$/) !== null) return 'required'
-    if(depositPublicKey.length < 66) return 'invalidKey'
+    if(depositPublicKey.length !== 66 && depositPublicKey.length !== 130) return 'invalid_public_key'
+    if (!/^[a-zA-Z0-9]*$/.test(depositPublicKey)) return 'invalid_public_key'
 }
 
 
