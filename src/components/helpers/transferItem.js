0e72007e28293a11cb593495131b1aac4b29953f
Evan Lynch <elynch@peerplays.com>
2022-06-24 11:11:01 -0400

    fix for multiline memo

--- src/components/helpers/transferItem.js
+++ src/components/helpers/transferItem.js
@@ -4,7 +4,7 @@ import parse from "html-react-parser";
 const TransferItem = ({data}) => (
     <div className="operation__row">
         <div className="title">{data.key}</div>
-        <div className="content">{parse(`${data.value}`)}</div>
+        <div className="content">{data.key == "memo" ? parse(`${data.value}`) : data.value}</div>
     </div>
 );
 
