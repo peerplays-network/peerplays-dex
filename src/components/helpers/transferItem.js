eb951ea296af6ea4b07e7ca724eb006d612e1561
dipak <dsavaliya@peerplays.com>
2022-07-12 11:59:48 +0530

    remove consoles

--- src/components/helpers/transferItem.js
+++ src/components/helpers/transferItem.js
@@ -5,7 +5,6 @@ const TransferItem = ({data}) => (
     <div className="operation__row">
         <div className="title"><Translate className="" content={`modal.recive.${data.key}`} /></div>
         <div className="content">
-            {console.log("tranferitem page",data.key === "memo" ?data.value.split("\n").map((line, index) => (<div key={index}>{line}</div>)):"55555")}
             {data.key === "memo" ? data.value.split("\n").map((line, index) => (<div key={index}>{line}</div>)) : data.value}
         </div>
     </div>
